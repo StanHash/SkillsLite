5200dc8c094257cdf379a17e20e276fb57091f37
Stan H <deindall@gmail.com>
2020-02-28 11:47:19 +0100

    MakeHack.sh update

--- MakeHack.sh
+++ MakeHack.sh
@@ -23,18 +23,21 @@ ea_exe="$base_dir/Tools/EventAssembler/ColorzCore.exe"
 textprocess_py="$base_dir/Tools/TextProcess/text-process-classic.py"
 parsefile_exe="$base_dir/Tools/EventAssembler/Tools/ParseFile"
 
-python3_exe="python3" # TODO
+# finding correct python version
+
+if hash python3 2> /dev/null; then
+  python3_exe="python3"
+elif hash python 2> /dev/null && [[ $(python -c 'import sys; print(int(sys.version_info[0] > 2))') -eq 1 ]]; then
+  python3_exe="python"
+else
+  echo "MakeHack.sh requires python 3 to be installed!" 1>&2
+  exit 1
+fi
 
 # do the actual building
 
-cd "$base_dir"
-
-echo "Preparing ROM"
-
-cp -f "$source_rom" "$target_rom" || exit 2
-
 if [[ $1 != quick ]]; then
-  # make hack full
+  # make hack full: process other files before assembling
 
   echo "Processing text"
 
@@ -42,10 +45,18 @@ if [[ $1 != quick ]]; then
   # all paths are abs so wd shouldn't matter otherwise
   cd "$(dirname "$text_main")"
 
-  $python3_exe "$textprocess_py" \
-    "$(basename "$text_main")" --installer "$text_installer" --definitions "$text_definitions" --parser-exe "$parsefile_exe"
+  $python3_exe "$textprocess_py" "$(basename "$text_main")" \
+    --installer "$text_installer" \
+    --definitions "$text_definitions" \
+    --parser-exe "$parsefile_exe"
 
 fi
+
+cd "$base_dir"
+
+echo "Preparing ROM"
+
+cp -f "$source_rom" "$target_rom" || exit 2
 
 echo "Assembling"
 
